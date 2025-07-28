ce6d3d27852ee1a22c73e358d56af1b4092176fd
dacdi_privat <dacdi_privat@MacBook-Air-von-Philipp.local>
2025-07-28 16:46:53 +0200

    feat: CLI-Integration für WMS-Starkregenkarten-Download via 'download-layers'-Subkommando

--- temp.py
+++
@@ -1,8 +0,0 @@
-from owslib.wms import WebMapService
-
-url = "https://sgx.geodatenzentrum.de/wms_starkregen"
-wms = WebMapService(url, version="1.3.0")
-
-print("\nVerfügbare Layer:")
-for layer in wms.contents:
-    print("-", layer)
