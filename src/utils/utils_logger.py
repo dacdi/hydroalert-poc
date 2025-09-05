c81b994b1253c79518bed6b78a469343d1e2a847
dacdi <david.muehlfeld@gmx.de>
2025-09-05 14:13:22 +0200

    git rm --cached -r output/ git commit -m "chore: remove output logs from repo and add to .gitignore" git push

--- src/utils/utils_logger.py
+++ src/utils/utils_logger.py
@@ -6,6 +6,7 @@ from src.config.config import LOG_FILE_PATH, TERMINAL_LOG_LEVEL, FILE_LOG_LEVEL
 
 _logger = None  # Singleton-Loggerinstanz für das gesamte Projekt
 
+
 def get_logger() -> logging.Logger:
     """
     Gibt den zentral konfigurierten Root-Logger zurück.
@@ -23,7 +24,8 @@ def get_logger() -> logging.Logger:
     terminal_handler = logging.StreamHandler()
     terminal_handler.setLevel(TERMINAL_LOG_LEVEL)
     terminal_formatter = logging.Formatter(
-        "%(asctime)s [%(levelname)s] %(filename)s:%(funcName)s - %(message)s", datefmt="%H:%M:%S"
+        "%(asctime)s [%(levelname)s] %(filename)s:%(funcName)s - %(message)s",
+        datefmt="%H:%M:%S",
     )
     terminal_handler.setFormatter(terminal_formatter)
     _logger.addHandler(terminal_handler)
@@ -34,9 +36,15 @@ def get_logger() -> logging.Logger:
     file_handler.setLevel(FILE_LOG_LEVEL)
     file_formatter = logging.Formatter(
         "%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d:%(funcName)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S"
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
     file_handler.setFormatter(file_formatter)
     _logger.addHandler(file_handler)
+
+    # Externe Bibliotheken auf WARNING setzen, damit keine Tokens im Log landen
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    logging.getLogger("telegram").setLevel(logging.WARNING)
+    logging.getLogger("aiogram").setLevel(logging.WARNING)
+    logging.getLogger("telethon").setLevel(logging.WARNING)
 
     return _logger
